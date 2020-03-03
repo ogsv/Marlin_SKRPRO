4527d7e1fb5757c2a4fe1866942b82fee2291baf
Ole Gunnar Sveen <ole.gunnar.sveen@cern.ch>
2020-03-03 10:13:00 +0100

    enabled case lighting on heater 2 pin

--- Marlin/src/pins/stm32/pins_BTT_SKR_PRO_V1_1.h
+++ Marlin/src/pins/stm32/pins_BTT_SKR_PRO_V1_1.h
@@ -172,9 +172,9 @@
 //
 #define HEATER_0_PIN       PB1  // Heater0
 #define HEATER_1_PIN       PD14 // Heater1
-#define HEATER_2_PIN       PB0  // Heater1
+//#define HEATER_2_PIN       PB0  // Heater2
 #define HEATER_BED_PIN     PD12 // Hotbed
-//#define FAN_PIN            PC8  // Fan0
+//#define FAN_PIN            PC8  // Fan0 
 //#define FAN1_PIN           PE5  // Fan1
 #define FAN_PIN           PE6  // Fan2
 
